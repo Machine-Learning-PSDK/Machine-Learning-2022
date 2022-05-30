3aa8468eba2d4eaff73bb9b3b1eec7b5cdbdeb95
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-05-30 23:41:52 +0200

    Added Comments and Pull Request Example
    - Added detailed comments, please try add similar level of description
    
    - Added a way to comment your functions, please use it as often as possible to help us collab easily.
    
    - Added an example of how to print your output for a pull request, so that someone can easily see that it does actually work without writing tests.

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,100 @@
-import data_cleaning
-import processing
-import prediction
+from posixpath import relpath
+from venv import create
+
+# Params: List<String>
+# Returns: List<Float>, List<Float>
+# Function: Convert list of scientific notation inputs to list of floats
+def convertLineToDataPointAndMemoryList(line):
+    zeroes_list = []  # Used to store initialized memory list as array of zeroes
+    # Convert each element to list of floats from scientific notation
+    for j in range(len(line)):
+        exponent = float(line[j][-2:])
+        num = float(line[j][0:19])
+        val = num * 10 ** (exponent)  # Converts scientific notation to float
+        val_list.append(val)  # append value onto list of datapoint elements
+        zeroes_list.append(0.0)  # Just to store size
+
+    return [  # Returns line converted to float list, and a zero array of the same size
+        val_list,
+        zeroes_list,
+    ]
+
+
+# Params: (List<<List<float>>>, <float>)
+# Returns: List<float>
+# Function: Return list representing redundant data point indexes as zeroes.
+def createMemoryList(dataset_input, memory_list):
+    # Iterate through each data point's elements
+    for i in range(len(dataset_input)):
+        for j in range(len(dataset_input[i])):
+            # Don't update element in memory list if it is equal to zero
+            if dataset_input[i][j] != 0:
+                memory_list[j] = dataset_input[i][j]
+
+    return memory_list
+
+
+# Params: List<List<FLoat>>, List<Float>
+# Returns: List<List<FLoat>>
+# Function: Remove corresponding redundant indeces from each datapoint
+def removeRedundantData(dataset_input, memory_list):
+    # Declare relevant Dataset
+    relevant_data = []
+    # Iterate through dataset
+    for i in range(len(dataset_input)):
+        relevant_data_point = []
+        for j in range(len(dataset_input[i])):
+            # If the memory list for datapoints at this index is 0, append it to the relevant datapoint list
+            if memory_list[j] != 0:
+                relevant_data_point.append(dataset_input[i][j])
+        relevant_data.append(relevant_data_point)
+    return relevant_data
+
+
+# Functions Ends
+# Pipeline Starts
+
+# Declarations.
+dataset_input = []
+dataset_class = []
+count = 0  # count number of rows we've ran through
+
+with open("inputs.txt") as file:
+
+    memory_list = []  # Just to store the memory list, indexes to keep
+    for lines in file:  # Iterate through row by row
+        line = lines.split()  # Remove spaces
+        val_list = []
+        # Convert line to array and initialize memory list for single data point
+        converted = convertLineToDataPointAndMemoryList(line)
+        # Array of zeroes and non-zeroes representing redundancy
+        memory_list = converted[1]  # Initializes length of mem list to match data point
+        val_list = converted[0]
+        dataset_input.append(val_list)  # append each word as element in datapoint
+        # print("Line:", count, "of 2000")
+        count += 1
+    # Create list of redundant data
+    memory_list = createMemoryList(dataset_input, memory_list)
+
+    # Remove the redundant rows from memory list.
+
+    relevant_data = removeRedundantData(dataset_input, memory_list)
+
+# Pull Request template, What was your task, how do you know it is working? Show on terminal
+print("Please print a test of your code when committing, as follows")
+print("TODO: Removing redundant data from data set")
+print("Input: Full dataset;", "Expected output: Full Data > Relevant Data ")
+print(
+    "Full dataset length:",
+    len(dataset_input[0]),
+    "; Relevant dataset length:",
+    len(relevant_data[0]),
+)
+
+# print("Memory list length:", len(memory_list))
+# print("Relevant datapoint length:", len(relevant_data[1000]))
+# print("Length of full dataset:", len(dataset_input))
+# print("Length of datapoint:", len(dataset_input[0]))
+# print("Datapoint row 0 zolumn zero:", dataset_input[0][0])
+
+# Pipeline Ends
